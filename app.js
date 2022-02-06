2e70d41f559f1bd9c9eed2e93f0112aba24159ed
Tuscanny Polk <tuscannypolk@gmail.com>
2022-02-06 13:47:54 -0500

    working

--- app.js
+++ app.js
@@ -3,7 +3,11 @@
 const inputBox = document.getElementById('mainInput');
 const createButton = document.getElementById('mainButton');
 const notesHolder = document.getElementById('notesHolder');
+const seeMoreButton = document.getElementById('seeMoreButton')
+let newButton;
 let inputBoxValue;
+let newDiv;
+
 
 
 //FUNCTIONS
@@ -19,17 +23,32 @@ console.log(inputBoxValue)
 const addValueToCard = () => {
 
     submitHandler();
-    let newDiv= document.createElement('div');
+    let firstNewDiv= document.createElement('div');
+    firstNewDiv.classList.add('eachNote');
+    
+    newDiv= document.createElement('div');
     newDiv.classList.add('note');
+    firstNewDiv.appendChild(newDiv);
     let newParagraph = document.createElement('p')
     newParagraph.classList.add('noteText');
     newParagraph.innerHTML = inputBoxValue;
     newParagraph.contentEditable = true;
     newDiv.appendChild(newParagraph);
-    notesHolder.appendChild(newDiv);
+    notesHolder.appendChild(firstNewDiv);
+    newButton = document.createElement("button");
+    newButton.classList.add('seeMoreButton')
+    newButton.innerHTML = "See more";
+    firstNewDiv.appendChild(newButton)
+    console.log(seeMoreButton)
     
+}
+
+const noteClick = () => {
+
+    alert('Note clicked');
 }
 
 //LISTENERS
 
 createButton.addEventListener('click', addValueToCard);
+seeMoreButton.addEventListener('click', function() {alert('See More')})
